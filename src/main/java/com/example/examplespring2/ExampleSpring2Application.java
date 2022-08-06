cc8dce6e0a6b4fa62c0cb8b6a936f78b41a1ff08
study-cho <dmcho.ex@gmail.com>
2022-08-06 16:25:21 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/examplespring2/ExampleSpring2Application.java
@@ -0,0 +1,13 @@
+package com.example.examplespring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpring2Application.class, args);
+    }
+
+}
